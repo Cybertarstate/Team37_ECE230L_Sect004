d3fbd253b81ae356b10449da33e381d19d7e3833
michaeldeur <91447929+michaeldeur@users.noreply.github.com>
2025-11-23 21:17:13 -0700

    Add files via upload

--- Lab13/onehot.v
+++ Lab13/onehot.v
@@ -1,1 +1,55 @@
-// Implement one-hot state machine
+module onehot(
+    input w,
+    input clk,
+    input reset,
+    output z, Astate, Bstate, Cstate, Dstate, Estate
+);
+    wire Anext, Bnext, Cnext, Dnext, Enext;
+
+    dff Adff(
+        .Default(1'b1),
+        .D(Anext),
+        .clk(clk),
+        .reset(reset),
+        .Q(Astate)
+    );
+
+    dff Bdff(
+        .Default(1'b0),
+        .D(Bnext),
+        .clk(clk),
+        .reset(reset),
+        .Q(Bstate)
+    );
+
+    dff Cdff(
+        .Default(1'b0),
+        .D(Cnext),
+        .clk(clk),
+        .reset(reset),
+        .Q(Cstate)
+    );
+    
+    dff Ddff(
+        .Default(1'b0),
+        .D(Dnext),
+        .clk(clk),
+        .reset(reset),
+        .Q(Dstate)
+    );
+
+    dff Edff(
+        .Default(1'b0),
+        .D(Enext),
+        .clk(clk),
+        .reset(reset),
+        .Q(Estate)
+    );
+    assign z = Cstate | Estate;
+
+    assign Anext = 1'b0;
+    assign Bnext = (~w & Astate) | (~w & Dstate) | (~w & Estate);
+    assign Cnext = (~w & Bstate) | (~w & Cstate);
+    assign Dnext = (w & Astate) | (w & Bstate) | (w & Cstate);
+    assign Enext = (w & Dstate) | (w & Estate);
+endmodule
--- Lab13/top.v
+++ Lab13/top.v
@@ -5,6 +5,24 @@ module top(
     input btnU // reset
 );
 
-    // Hook up binary and one-hot state machines
+    binary inst1(
+        .w(sw),
+        .clk(btnC),
+        .reset(reset),
+        .z(led[1]),
+        .State(led[9:7])
+    );
+    
+    onehot inst2(
+        .w(sw),
+        .clk(btnC),
+        .reset(btnU),
+        .z(led[0]),
+        .Astate(led[2]),
+        .Bstate(led[3]),
+        .Cstate(led[4]),
+        .Dstate(led[5]),
+        .Estate(led[6])
+    );
 
 endmodule
